3b1cb3fd5016d145c885e7048f6aedbf5fe2fa2e
serswik <megasergik@icloud.com>
2025-10-22 15:07:50 +0300

    Created models folder and added user class

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+namespace DatabaseApp.Models
+{
+    public class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
