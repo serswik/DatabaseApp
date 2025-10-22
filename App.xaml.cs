4d1190f85dc90a200c1f955ddbf09ad94efaa9f6
serswik <megasergik@icloud.com>
2025-10-22 14:51:11 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DatabaseApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
