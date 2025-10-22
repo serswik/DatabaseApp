4864a2bae0dd1def64c3f4321b7e69a9af45b665
serswik <megasergik@icloud.com>
2025-10-22 15:12:24 +0300

    Created services folder and added Database service

---
+++ Services/DatabaseService.cs
@@ -0,0 +1,26 @@
+﻿using SQLite;
+using DatabaseApp.Models;
+
+namespace DatabaseApp.Services
+{
+    public class DatabaseService
+    {
+        private readonly SQLiteAsyncConnection _db;
+
+        public DatabaseService(string dbPath)
+        {
+            _db = new SQLiteAsyncConnection(dbPath);
+            _db.CreateTableAsync<User>().Wait();
+        }
+
+        public Task<int> AddUserAsync(User user)
+        {
+            return _db.InsertAsync(user);
+        } 
+
+        public Task<List<User>> GetUsersAsync()
+        {
+            return _db.Table<User>().ToListAsync();
+        }
+    }
+}
