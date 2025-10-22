672487ff1147f58ee913f7b58790e795c595fda9
serswik <megasergik@icloud.com>
2025-10-22 22:03:56 +0300

    Changed structure in MainPageViewModel; Added commands for deleting and editing users in UsersListViewModel; Added buttons to edit/delete on UserListPage

--- ViewModels/UsersListViewModel.cs
+++ ViewModels/UsersListViewModel.cs
@@ -26,6 +26,7 @@ namespace DatabaseApp.ViewModels
         {
             var list = await _dbService.GetUsersAsync();
 
+            Users.Clear();
             foreach (var user in list)
             {
                 if (!Users.Any(u => u.Id == user.Id))
@@ -33,6 +34,32 @@ namespace DatabaseApp.ViewModels
                     Users.Add(user);
                 }
             }
+        }
+
+        [RelayCommand]
+        public async Task DeleteUserAsync(User user)
+        {
+            bool confirm = await Shell.Current.DisplayAlert(
+                "Confirm Delete",
+                $"Are you sure you want to delete {user.Name}?",
+                "Yes", "No");
+
+            if (!confirm) return;
+
+            await _dbService.DeleteUserAsync(user);
+            Users.Remove(user);
+        }
+
+        [RelayCommand]
+        public async Task EditUserAsync(User user)
+        {
+            var navParams = new Dictionary<string, object>
+            {
+                { "UserToEdit", user }
+            };
+            await Shell.Current.GoToAsync("///main-page", navParams);
+
+            await LoadUsersAsync();
         }
     }
 }
