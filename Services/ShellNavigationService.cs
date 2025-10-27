c04e176f434c474a6f837c44492f240563c837ea
serswik <megasergik@icloud.com>
2025-10-27 14:19:55 +0200

    Added methods in ShellNavigationService

--- Services/ShellNavigationService.cs
+++ Services/ShellNavigationService.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using DatabaseApp.Interfaces;
 namespace DatabaseApp.Services
 {
-    internal class ShellNavigationService
+    public class ShellNavigationService : INavigationService
     {
+        public async Task GoToAsync(string route, IDictionary<string, object>? parameters = null)
+        {
+            if (parameters != null)
+            {
+                await Shell.Current.GoToAsync(route, parameters);
+            }
+            else
+            {
+                await Shell.Current.GoToAsync(route);
+            }
+        }
+
+        public async Task GoBackAsync()
+        {
+            await Shell.Current.GoToAsync("..");
+        }
     }
 }
